139c250a8c6105db8d8d9ef42eac3a15d4fe17c3
jereimer <jens@reimer-warendorf.de>
2017-05-23 10:27:04 +0200

    H angepasst

--- RingBuffer.h
+++ RingBuffer.h
@@ -1,3 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// File: RingBuffer.h
+//
+// Ingenieurorientierte Programmierung
+// Dr. Alexander Kling
+//
+// Carsten Bevermann
+// Christoph Geitner
+// Jens Reimer
+// Marc Töpker
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef RINGBUFFER_H
 #define RINGBUFFER_H
 
@@ -6,7 +20,9 @@ template <class T> RingBuffer
     size_t d_size;                          // Number of Data
     //bool overwrite;                       // sollte überflüssig sein
     T* d_buf;                               // Data Buffer
-    T* d_next;                              // Pointer to next data location
+    T* d_next;
+    T* d_read_next;
+                                 // Pointer to next data location
 public:
     RingBuffer();                           // Default-Constructor
     RingBuffer(size_t);                     // Custom-Constructor
