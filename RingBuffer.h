b2260b56652278fb073cb0b576f1a67dbb4427f1
MarcToepker <marc.toepker@hs-osnabrueck.de>
2017-05-23 10:10:01 +0200

    Added Files

--- RingBuffer.h
+++ RingBuffer.h
@@ -1,18 +1,18 @@
 template <class T> RingBuffer
 {
-    size_t d_size;      // Number of Data
-    T* d_buf;           // Data Buffer
-    T* d_next;          // Pointer to next data location
+    size_t d_size;                          // Number of Data
+    //bool overwrite;                       // sollte überflüssig sein
+    T* d_buf;                               // Data Buffer
+    T* d_next;                              // Pointer to next data location
 public:
-    RingBuffer();       // Default-Constructor
-    RingBuffer(size_t); // Custom-Constructor
-    RingBuffer(const RingBuffer&)   // Copy-Constructor
-    ~RingBuffer();      // Destructor
+    RingBuffer();                           // Default-Constructor
+    RingBuffer(size_t);                     // Custom-Constructor
+    RingBuffer(const RingBuffer&)           // Copy-Constructor
+    ~RingBuffer();                          // Destructor
 
-    void storeData(T);  // store data element
-    T readData();       // read data element
-    bool isEmpty() const; // empty RingBuffer ?
-    void eraseBuffer();     // erase data from buffer
+    void storeData(T);                      // store data element
+    T readData();                           // read data element
+    bool isEmpty() const;                   // empty RingBuffer ?
+    void eraseBuffer();                     // erase data from buffer
     RingBuffer& operator= (RingBuffer&);    // overload assignment operator
-
 }
